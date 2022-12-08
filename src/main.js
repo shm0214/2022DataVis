fbd8b48ccadbd1759b9fcc9255b156e50e90f18e
shm0214 <1186562408@qq.com>
2022-12-08 23:38:42 +0800

    left ok!

--- src/main.js
+++ src/main.js
@@ -2,7 +2,40 @@
 import * as d3 from "d3";
 import histogram from "./histogram.js";
 import $ from "jquery";
-import total from "../data/AQI-rank/total.csv"
+import total from "../data/AQI-rank/total.csv";
+import rank_11 from "../data/AQI-rank/11.csv";
+import rank_12 from "../data/AQI-rank/12.csv";
+import rank_13 from "../data/AQI-rank/13.csv";
+import rank_14 from "../data/AQI-rank/14.csv";
+import rank_15 from "../data/AQI-rank/15.csv";
+import rank_21 from "../data/AQI-rank/21.csv";
+import rank_22 from "../data/AQI-rank/22.csv";
+import rank_23 from "../data/AQI-rank/23.csv";
+import rank_31 from "../data/AQI-rank/31.csv";
+import rank_32 from "../data/AQI-rank/32.csv";
+import rank_33 from "../data/AQI-rank/33.csv";
+import rank_34 from "../data/AQI-rank/34.csv";
+import rank_35 from "../data/AQI-rank/35.csv";
+import rank_36 from "../data/AQI-rank/36.csv";
+import rank_37 from "../data/AQI-rank/37.csv";
+import rank_41 from "../data/AQI-rank/41.csv";
+import rank_42 from "../data/AQI-rank/42.csv";
+import rank_43 from "../data/AQI-rank/43.csv";
+import rank_44 from "../data/AQI-rank/44.csv";
+import rank_45 from "../data/AQI-rank/45.csv";
+import rank_46 from "../data/AQI-rank/46.csv";
+import rank_50 from "../data/AQI-rank/50.csv";
+import rank_51 from "../data/AQI-rank/51.csv";
+import rank_52 from "../data/AQI-rank/52.csv";
+import rank_53 from "../data/AQI-rank/53.csv";
+import rank_54 from "../data/AQI-rank/54.csv";
+import rank_61 from "../data/AQI-rank/61.csv";
+import rank_62 from "../data/AQI-rank/62.csv";
+import rank_63 from "../data/AQI-rank/63.csv";
+import rank_64 from "../data/AQI-rank/64.csv";
+import rank_65 from "../data/AQI-rank/65.csv";
+import rank_71 from "../data/AQI-rank/71.csv";
+import rank_81 from "../data/AQI-rank/81.csv";
 
 const colorMap = new Map();
 colorMap.set("PM2.5", "rgb(66,133,244)");
@@ -12,19 +45,142 @@ colorMap.set("NO2", "rgb(237,99,37)");
 colorMap.set("CO", "rgb(237,99,37)");
 colorMap.set("O3", "rgb(202,156,44)");
 
-
-d3.csv(total).then((data, error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(data);
-        var width = $(".left-down").width();
-        var height = $(".left-down").height();
-        histogram(data, {
-            width: width,
-            height: height,
-            colorMap: colorMap,
-            num: 20,
-        });
-    }
+$("#range").on("change", function (e) {
+    var val = $("#range").val();
+    $("#num").attr("placeholder", val);
+    num = parseInt(val);
+    left_render();
 });
+
+$("#select").on("change", function (e) {
+    var val = $("#select").val();
+    switch (val) {
+        case "total":
+            csv = total;
+            break;
+        case "11":
+            csv = rank_11;
+            break;
+        case "12":
+            csv = rank_12;
+            break;
+        case "13":
+            csv = rank_13;
+            break;
+        case "14":
+            csv = rank_14;
+            break;
+        case "15":
+            csv = rank_15;
+            break;
+        case "21":
+            csv = rank_21;
+            break;
+        case "22":
+            csv = rank_22;
+            break;
+        case "23":
+            csv = rank_23;
+            break;
+        case "31":
+            csv = rank_31;
+            break;
+        case "32":
+            csv = rank_32;
+            break;
+        case "33":
+            csv = rank_33;
+            break;
+        case "34":
+            csv = rank_34;
+            break;
+        case "35":
+            csv = rank_35;
+            break;
+        case "36":
+            csv = rank_36;
+            break;
+        case "37":
+            csv = rank_37;
+            break;
+        case "41":
+            csv = rank_41;
+            break;
+        case "42":
+            csv = rank_42;
+            break;
+        case "43":
+            csv = rank_43;
+            break;
+        case "44":
+            csv = rank_44;
+            break;
+        case "45":
+            csv = rank_45;
+            break;
+        case "46":
+            csv = rank_46;
+            break;
+        case "50":
+            csv = rank_50;
+            break;
+        case "51":
+            csv = rank_51;
+            break;
+        case "52":
+            csv = rank_52;
+            break;
+        case "53":
+            csv = rank_53;
+            break;
+        case "54":
+            csv = rank_54;
+            break;
+        case "61":
+            csv = rank_61;
+            break;
+        case "62":
+            csv = rank_62;
+            break;
+        case "63":
+            csv = rank_63;
+            break;
+        case "64":
+            csv = rank_64;
+            break;
+        case "65":
+            csv = rank_65;
+            break;
+        case "71":
+            csv = rank_71;
+            break;
+        case "81":
+            csv = rank_81;
+            break;
+    }
+    left_render();
+});
+
+var csv = total;
+var num = 20;
+
+function left_render() {
+    $(".svg-left-down").empty();
+    d3.csv(csv).then((data, error) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(data);
+            var width = $(".left-down").width();
+            var height = $(".left-down").height();
+            histogram(data, {
+                width: width,
+                height: height,
+                colorMap: colorMap,
+                num: num,
+            });
+        }
+    });
+}
+
+left_render();
